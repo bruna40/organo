d2afff76bf6d94b0c88e4f0ef61336f4e86b96d3
Bruna Santiago <brunasanthiago@outlook.com>
2022-10-04 13:13:16 -0300

    css do formulario pronto

--- organograma/src/components/Forms/index.js
+++ organograma/src/components/Forms/index.js
@@ -1,22 +1,26 @@
 import './index.css'
+import CampoTexto from '../CampoTexto/index';
 
 export default function Forms() {
+  
   return (
-    <div className="campo-texto">
-        <h2>Preencha os dados para criar o card do colaborador</h2>
-        <form> 
-            <label htmlFor="name">Nome</label>
-            <input type="text" id="name" name="name" placeholder="Digite seu nome" />
-            <label htmlFor="Cargo">Cargo</label>
-            <input type="text" id="cargo" name="cargo" placeholder="Digite seu cargo" />
-            <label htmlFor="imagem">Imagem</label>
-            <input type="text" id="imagem" name="imagem" placeholder="Informe o endereço da imagem" />
-            <label htmlFor="time">Time</label>
-            <input type="text" id="time" name="time" placeholder="Informe o time" />
-
-            <button type="submit">Criar Card</button>
+    <section className="formulario">
+        <form>
+            <h2>Preencha os dados para criar o card do colaborador</h2>
+            <CampoTexto
+                label="Nome"
+                placeholder="Digite seu nome"
+            />
+            <CampoTexto
+                label="Cargo"
+                placeholder="Digite seu cargo"
+            />
+            <CampoTexto
+                label="Imagem"
+                placeholder="Cole a URL da imagem"
+            />
             
         </form>
-    </div>
+    </section>
   );
 }
